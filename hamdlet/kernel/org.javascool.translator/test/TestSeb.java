7053a0776f22f8ed3904cea1733d78232a3daa58
vthierry <vthierry@79b98d2d-744f-0410-929f-8ac7b80d50f7>
2009-06-17 10:18:21 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/javascool@74 79b98d2d-744f-0410-929f-8ac7b80d50f7

--- hamdlet/kernel/org.javascool.translator/test/TestSeb.java
+++
@@ -1,14 +0,0 @@
-import static javascool.Macro.*;
-import java.util.ArrayList;
-public class TestSeb{
-public static int a;
-
-
-public static int fct (int a, int b){
- return a+b;
-}
-
-
-int t;
-}
-
