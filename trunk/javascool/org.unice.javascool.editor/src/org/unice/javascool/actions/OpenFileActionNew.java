70e9af7855177a597a0d9d873e5333fb6c9d4e83
chalmeton <chalmeton@79b98d2d-744f-0410-929f-8ac7b80d50f7>
2009-06-17 20:35:48 +0000

    new open File action
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/javascool@82 79b98d2d-744f-0410-929f-8ac7b80d50f7

---
+++ trunk/javascool/org.unice.javascool.editor/src/org/unice/javascool/actions/OpenFileActionNew.java
@@ -0,0 +1,79 @@
+package org.unice.javascool.actions;
+
+import java.io.File;
+import java.text.MessageFormat;
+
+import org.eclipse.core.filesystem.EFS;
+import org.eclipse.core.filesystem.IFileStore;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.action.IMenuCreator;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.util.IPropertyChangeListener;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.HelpListener;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.actions.ActionFactory.IWorkbenchAction;
+import org.eclipse.ui.ide.FileStoreEditorInput;
+import org.eclipse.ui.texteditor.IWorkbenchActionDefinitionIds;
+import org.unice.javascool.editor.editors.JVSEditor;
+
+public class OpenFileActionNew implements IWorkbenchWindowActionDelegate {
+
+	private IWorkbenchWindow window;
+
+	
+	@Override
+	public void init(IWorkbenchWindow window) {
+		this.window = window;
+	}
+
+	@Override
+	public void run(IAction action) {
+		FileDialog fd = new FileDialog(window.getShell(), SWT.OPEN);
+		fd.setFilterExtensions(new String[] {"*.jvs", "*.java"});
+		fd.setText("Open...");
+
+		IEditorPart editor = null;
+		if(fd.open() != null) {
+
+			File file = new File(fd.getFilterPath() + "/" + fd.getFileName());
+			//IWorkbenchPage page= PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+			if (file != null) {
+				IWorkbenchPage page= window.getActivePage();
+				try {
+					IFileStore fileStore= EFS.getStore(file.toURI());
+					page.openEditor(new FileStoreEditorInput(fileStore), JVSEditor.ID);
+				} catch (CoreException e) {
+					e.printStackTrace();
+				}
+			} else if (file != null) {
+				String msg = MessageFormat.format("File is null: {0}", new String[] { file.getName() }); //$NON-NLS-1$
+				MessageDialog.openWarning(window.getShell(), "Probleme", msg); //$NON-NLS-1$
+			}
+		}
+	}
+
+	@Override
+	public void selectionChanged(IAction action, ISelection selection) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+
+
+}
