4b8c1a87f48d1bb97498a619ff7e3e2220ba2181
hamdi <hamdi@79b98d2d-744f-0410-929f-8ac7b80d50f7>
2009-08-03 11:30:29 +0000

    delete LyceeGamlet
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/javascool@152 79b98d2d-744f-0410-929f-8ac7b80d50f7

--- javascool_web_V1/trunk/LyceeGamlet/MyClass.java
+++
@@ -1,4 +0,0 @@
-       package compiler;      public class MyClass {    public void hello() {         System.out.println("Hello tous le monde ! "); }}
-       package compiler;      public class MyClass {    public void hello() {         System.out.println("Hello tous le monde ! "); }}
-       public class MyClass {    public void hello() {         System.out.println("Hello tous le monde ! "); }}
-       public class MyClass {    public void hello() {         System.out.println("Hello tous le monde ! "); }}
