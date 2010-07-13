3f75c3e5c9e998844c0a69c63734363ea2f33c51
philien <philien@79b98d2d-744f-0410-929f-8ac7b80d50f7>
2010-07-13 14:25:23 +0000

    Project Creation
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/javascool@454 79b98d2d-744f-0410-929f-8ac7b80d50f7

---
+++ javascool_V3/trunk/org.javascool/src/org/javascool/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.javascool;
+
+/**
+ * @author Philippe Vienne
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+			
+	}
+
+}
