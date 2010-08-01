799256c8a1e1d1e34f68a0d731e2c3cbe6430c17
vthierry <vthierry@79b98d2d-744f-0410-929f-8ac7b80d50f7>
2010-08-01 12:41:07 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/javascool@495 79b98d2d-744f-0410-929f-8ac7b80d50f7

---
+++ javascool_V3/trunk/org.javascool/proglet/ingredients/Main.java
@@ -0,0 +1,214 @@
+/*******************************************************************************
+ * Thierry.Vieville@sophia.inria.fr, Copyright (C) 2009.  All rights reserved. *
+ *******************************************************************************/
+
+package org.javascool.old;
+
+// Used to define the gui
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import javax.swing.BorderFactory;
+
+// Used for the read/write
+import javax.swing.JLabel;
+import javax.swing.JEditorPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/** Définit une proglet javascool qui permet de faire des entrées/sorties au clavier.
+ * @see <a href="Konsol.java">code source</a>
+ */
+public class Konsol implements Proglet { private Konsol() { }
+  private static final long serialVersionUID = 1L;
+
+  // This defines the panel to display
+  private static class Panel extends JPanel {
+    private static final long serialVersionUID = 1L;
+
+    public Panel() {
+      super(new BorderLayout()); 
+      setBackground(Color.WHITE); setPreferredSize(new Dimension(400, 500));
+      out = new JEditorPane(); out.setEditable(false);  out.setContentType("text/html; charset=UTF-8");
+      pane = new JScrollPane(); pane.setViewportView(out); pane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS); add(pane);
+      pane.setBorder(BorderFactory.createTitledBorder("output")); 
+      JPanel bar = new JPanel(); add(bar, BorderLayout.NORTH);    
+      prompt = new JLabel("input>"); bar.add(prompt);
+      in = new JTextField(); in.setEditable(false); in.setPreferredSize(new Dimension(300, 30)); bar.add(in);    
+      in.addActionListener(new ActionListener() { public void actionPerformed(ActionEvent e) {
+	// Reads the input text and reset the interface
+	input = ((JTextField) e.getSource()).getText(); in.setEditable(false);
+      }});
+      JButton clear = new JButton("clear output"); add(clear, BorderLayout.SOUTH);
+      clear.addActionListener(new ActionListener() { public void actionPerformed(ActionEvent e) {
+	clear();
+      }});
+    }
+
+    /** Writes a string.
+     * @param string The string to write.
+     * @param html If true writes in html else in plain text
+     */
+    public void writeString(String string, boolean html) {
+      output += (html ? string : quote(string));
+      out.setText("<html><body>"+output+"</body></html>"); 
+      pane.getVerticalScrollBar().setValue(pane.getVerticalScrollBar().getMaximum());
+    }
+    private JEditorPane out; private JScrollPane pane;
+
+    /** Clears the output. */
+    public void clear() {
+      out.setText(output = "");
+      prompt.setText("input>"); 
+      in.setText(""); 
+    }
+
+    /** Reads a string.
+     * @return The read string.
+     */
+    public String readString() {
+      // Interaction loop with in action listener
+      in.setText("");
+      in.setEditable(true); 
+      in.requestFocus();
+      try {
+	while(in.isEditable()) 
+	  Thread.sleep(100);
+      } catch(Exception e) {
+	in.setText("");
+	in.setEditable(false); 
+	prompt.setText("input>"); 
+	throw new RuntimeException("Program stopped !");
+      }
+      prompt.setText("input>"); 
+      return input == null ? "" : input;
+    }
+    private JLabel prompt; private JTextField in; private String input, output = "";
+  }
+
+  // Quotes a string for HTML
+  private static String quote(String string) { return string.replaceAll("&", "&amp;").replaceAll("<", "&lt;").replaceAll(" ", "&nbsp;"); }
+
+  //
+  // This defines the tests on the panel
+  //
+
+  /** Test du panel. */
+  static void test() {
+    clear();
+    println("Bonjour, qui es tu ?");
+    String nom = readString();
+    println ("Enchanté "+nom+" ! Quel age as tu ?");
+    int age = readInteger();
+    //for(int i = 0; i < 100; i++)
+    println("He je suis plus vieux que toi !!");
+  }
+
+  //
+  // This defines the javascool interface
+  //
+
+  /** Ecrit une chaine de caractères dans la fenêtre de sortie (output) et passe à la ligne.
+   * @param string La chaine à écrire.
+   */
+  public static void println(String string) { panel.writeString(string, false);  panel.writeString("<br/>\n", true); }
+  public static void println(long string) { println(""+string); }
+  public static void println(double string) { println(""+string); }
+  public static void println(boolean string) { println(""+string); }
+
+  /** Ecrit une chaine de caractères dans la fenêtre de sortie (output) et sans passer à la ligne.
+   * @param string La chaine à écrire.
+   */
+  public static void print(String string) { panel.writeString(string, false); }
+  public static void print(long string) { print(""+string); }
+  public static void print(double string) { print(""+string); }
+  public static void print(boolean string) { print(""+string); }
+
+  /** Ecrit une chaine de caractères colorée dans la fenêtre de sortie (output) et passe à la ligne.
+   * @param string La chaine à écrire.
+   * @param couleur La couleur: "black" (default), "blue", "cyan", "gray", "green", "magenta", "orange", "pink", "red", "white", "yellow".
+   */
+  public static void println(String string, String couleur) {
+    panel.writeString("<span style='color:"+couleur+"'>"+quote(string)+"</span>", true);
+  }
+
+  /** Ecrit une chaine de caractères avec des codes HTML dans la fenêtre de sortie (output).
+   * <br>-Pour écrire en couleur on peut par exemple utiliser: 
+   * <div><tt>printHtml("Oh, je vois &lt;span style='color:red;font-weight:bold'>rouge&lt;/span> !");</tt></div> 
+   * où le code HTML spécifie la couleur du texte en rouge (<tt>color:red</tt>) et d'utiliser des caractères gras (<tt>font-weight:bold</tt>).
+   * @param string La chaine à écrire.
+   */
+  public static void printHtml(String string) {
+    panel.writeString(string, true);
+  }
+
+  /** Efface tout ce qui est déjà écrit dans la console. */
+  public static void clear() {
+    panel.clear();
+  }
+
+  /** Lit une chaîne de caractère dans la fenêtre d'entrée (input).
+   * @return La chaîne lue.
+   */
+  public static String readString() {
+    return panel.readString();
+  }
+  
+  /** Lit un nombre entier dans la fenêtre d'entrée (input).
+   * @return Le nombre entier lu ou 0 en cas d'erreur.
+   * <br> Voir <a href="#readInt()">readInt</a> dont il est synonyme.
+   */
+  public static int readInteger() {
+    return (int) readFloat();
+  }
+  
+  /** Lit un nombre entier dans la fenêtre d'entrée (input).
+   * @return Le nombre entier lu ou 0 en cas d'erreur.
+   * <br> Voir <a href="#readInteger()">readInteger</a> dont il est synonyme.
+   */
+  public static int readInt() {
+    return readInteger();
+  }
+
+  /** Lit un nombre décimal dans la fenêtre d'entrée (input).
+   * @return Le nombre décimal lu ou 0 en cas d'erreur.
+   * <br> Voir <a href="#readDouble()">readDouble</a> dont il est synonyme.
+   */
+  public static double readFloat() {
+    while(true) {
+      String rep = panel.readString().replaceAll("([0-9]),([0-9])", "$1.$2");
+      try {
+	return Double.parseDouble(rep);
+      } catch(Exception e) { 
+	println("Erreur de saisie: ``"+rep+"´´ n'est pas un nombre");
+      }
+    }
+  }
+
+  /** Lit un nombre décimal dans la fenêtre d'entrée (input).
+   * @return Le nombre décimal lu ou 0 en cas d'erreur.
+   * <br> Voir <a href="#readFloat()">readFloat</a> dont il est synonyme.
+   */
+  public static double readDouble() {
+    return readFloat();
+  }
+
+  /** Lit un booléen dans la fenêtre d'entrée (input).
+   * @return Le booléen lu
+   */
+  public static boolean readBoolean() {
+    while(true) {
+      String rep = panel.readString().toLowerCase().trim();
+      if (rep.matches("(vrai|vraie|v|true|t|oui|o|yes|y|1|ok)")) return true;
+      if (rep.matches("(faux|fausse|f|false|non|n|no|0|ko)")) return false;
+      println("Erreur de saisie: ``"+rep+"´´ n'est pas un booleen (utiliser ``vrai´´ ou ``faux´´)");
+    }
+  }
+
+  /** Définition de l'interface graphique de la proglet. */
+  public static final Panel panel = new Panel();
+}
---
+++ javascool_V3/trunk/org.javascool/proglet/synthesons/Main.java
@@ -0,0 +1,79 @@
+/*******************************************************************************
+ * Thierry.Vieville@sophia.inria.fr, Copyright (C) 2009.  All rights reserved. *
+ *******************************************************************************/
+
+package org.javascool.old;
+
+// Used to define the gui
+import javax.swing.JPanel;
+import java.awt.Dimension;
+import java.awt.BorderLayout;
+
+// Used to define a button
+import javax.swing.JButton;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/** Définit une proglet javascool qui permet d'expérimenter avec des signaux sonores.
+ * @see <a href="Synthe.java">code source</a>
+ */
+public class Synthe implements Proglet { private Synthe() { }
+  private static final long serialVersionUID = 1L;
+
+  // This defines the panel to display
+  private static class Panel extends SoundBit.Panel {
+    private static final long serialVersionUID = 1L;
+    public SoundBit sound = new SoundBit.NotesSoundBit() {
+	public double get(char channel, double time) { return Synthe.tone == null ? Math.sin(2 * Math.PI * time) : Synthe.tone.get(channel, time); }
+      };
+    public Panel() { reset(sound, 'l'); sound.reset("16 A"); }
+  }
+
+  //
+  // This defines the tests on the panel
+  //
+
+  /** Test du panel. */
+  static void test() {
+    test(new SoundBit() { public double get(char c, double t) { return sns(t); } });
+    test(new SoundBit() { public double get(char c, double t) { return 0.5 * sqr(t); } });
+    test(new SoundBit() { public double get(char c, double t) { return 0.8 * tri(t) + 0.2 * noi(t); } });
+    test(new SoundBit() { public double get(char c, double t) { return 0.3 * sqr(t/2) * sns(t) + 0.3 * sns(2 * t) + 0.3 * tri(3 * t); } });
+    syntheSet("e5 e5b e5 e5b e5 e5b e5 b d5 c5 4 a | 1 h c e a 4 b | 1 h e g g# 4 a"); synthePlay();
+  } 
+  private static void test(SoundBit sound) {
+    tone = sound; syntheSet("16 a"); synthePlay(); 
+  }
+
+  //
+  // This defines the javascool interface
+  //
+
+  /** Definit le son à utiliser pour jouer les notes du synthétiseur. 
+   * <div>Utilisé à travers la <a href="../../doc/about-proglet-Synthe.htm">construction</a> <tt>Javascool</tt> de la forme <tt>TONE: <i>expression de la variable t</i></tt></div>
+   */
+  static public SoundBit tone = null;
+
+  /** Retourne la valeur d'une sinusoïde de période une seconde et d'amplitude unité. */
+  static public double sns(double t) { return Math.sin(2 * Math.PI * t); }
+
+  /** Retourne la valeur d'un signal carré de période une seconde et d'amplitude unité. */
+  static public double sqr(double t) { return (int) (t * 2) % 2 == 0 ? 1 : -1; }
+
+  /** Retourne la valeur d'un signal triangulaire de période une seconde et d'amplitude unité. */
+  static public double tri(double t) { return (int) (t * 2) % 2 == 0 ? (-1 + 4 * (t - (int) t)) : (3 - 4 * (t - (int) t)); }
+
+  /** Retourne la valeur d'un souffle (``bruit blanc´´). */
+  static public double noi(double t) { return 2 * Math.random() - 1; }
+
+  /** Définit les notes à jouer dans cet interface. 
+   * @param notes Définition des <a href="SoundBit.html#notes">notes</a> selon une syntaxe simplifiée.
+   */
+  static public void syntheSet(String notes) { panel.sound.reset(notes); panel.reset(panel.sound, 'l'); }
+
+  /** Fait entendre le son sur le système audio. */
+  static public void synthePlay() { try { panel.sound.play(); } catch(Exception e) { Macros.echo(e.toString()); } }
+
+  /** Définition de l'interface graphique de la proglet. */
+  public static final Panel panel = new Panel();
+}
