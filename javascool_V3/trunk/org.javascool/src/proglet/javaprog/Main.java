88748c26d506b90337b4bcaba32f1e06920b4d71
vthierry <vthierry@79b98d2d-744f-0410-929f-8ac7b80d50f7>
2010-08-01 16:32:16 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/javascool@500 79b98d2d-744f-0410-929f-8ac7b80d50f7

---
+++ javascool_V3/trunk/org.javascool/src/proglet/javaprog/Main.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Thievery.Vieville@sophia.inria.fr, Copyright (C) 2009.  All rights reserved. *
+ *******************************************************************************/
+
+package proglet.javaprog;
+
+// Used to define the gui
+import javax.swing.JPanel;
+import javax.swing.JLayeredPane;
+import java.awt.Dimension;
+
+/** Définit une proglet javascool qui permet d'utiliser toute les classes des swings.
+ * @see <a href="http://java.sun.com/docs/books/tutorial/uiswing">Java Swing tutorial</a>
+ * @see <a href="http://java.sun.com/javase/6/docs/api/javax/swing/package-summary.html">Java Swing API</a>
+ * @see <a href="../javaprog/Main.java">code source</a>
+ */
+public class Main implements org.javascool.Proglet { private Main() { }
+  private static final long serialVersionUID = 1L;
+
+  // This defines the panel to display
+  private static class Panel extends JPanel {
+    private static final long serialVersionUID = 1L;
+
+    public Panel() {
+       pane = new JLayeredPane(); pane.setPreferredSize(new Dimension(width, height)); add(pane);
+    }
+    JLayeredPane pane;
+    static final int width = 512, height = 512; 
+  }
+
+  //
+  // This defines the tests on the panel
+  //
+
+  /** Test du panel. */
+  static void test() {
+  }
+
+  //
+  // This defines the javascool interface 
+  //
+
+  /** Renvoie le panneau. */
+  public static JLayeredPane getSwingPane() {
+    return panel.pane;
+  }
+
+  /** Définition de l'interface graphique de la proglet. */
+  public static final Panel panel = new Panel();
+}
