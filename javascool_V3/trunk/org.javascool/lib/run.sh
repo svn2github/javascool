2e70ce0c5ffcd8943a01b3e9008e3721efca02ad
vthierry <vthierry@79b98d2d-744f-0410-929f-8ac7b80d50f7>
2010-08-04 10:16:41 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/javascool@517 79b98d2d-744f-0410-929f-8ac7b80d50f7

---
+++ javascool_V3/trunk/org.javascool/lib/run.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Usage of this script
+if [ -z "$1" -o "$1" = "-h" -o "$1" = "--help" ] 
+then cat <<EOD
+Usage: $0 <main> [arguments]
+ Runs one of the org.javascool main 
+Usage: $0 sax -o <out-file> <xml-file> <xsl-file>
+ Runs a XML to XML translation usng a XSL
+EOD
+exit 0
+fi
+
+# Retrieve javascool root directory and main class
+if echo $0 | grep '^/' ; then root="$0" ; else root="`pwd`/$0" ; fi ; root=`echo $root | sed 's/\/lib\/run.sh$//' | sed 's/\/lib\/\.\/run.sh$//'`
+main=`echo $1 | sed 's/^org\.javascool\.//'` ; shift
+
+# Running the saxon translator
+if [ "$main" = sax ] ; then java -jar $root/lib/saxon.jar $* ; exit $? ; fi
+
+# Running the class
+echo "Compiling and running the org.javascool class $main from $root"
+make -s -C $root cmp ; java -cp $root/src:$root/lib/tools.jar:$root/lib/saxon.jar org.javascool.$main $*
+
+
