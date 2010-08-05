66e9aa5e0e2b3a21a6231787eabe4a2eccffb8be
philien <philien@79b98d2d-744f-0410-929f-8ac7b80d50f7>
2010-08-05 10:10:08 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/javascool@536 79b98d2d-744f-0410-929f-8ac7b80d50f7

--- javascool_V3/trunk/org.javascool/src/org/javascool/Main.java
+++ javascool_V3/trunk/org.javascool/src/org/javascool/Main.java
@@ -14,18 +14,31 @@ import javax.swing.JTabbedPane;
 import javax.swing.ImageIcon;
 import javax.swing.JComponent;
 import javax.swing.JSplitPane;
+import javax.swing.*; 
 
 import java.lang.String;
 import java.awt.BorderLayout;
+import java.awt.event.*;
+//Java
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.SwingUtilities;
+import javax.swing.filechooser.*;
+
 
 /** This is the javascool v3 interface starter.
  * <p>- It can be used either as standalone application or a certified applet.</p>
  * @author Philippe Vienne
  * @see <a href="../../org/javascool/Main.java">source code</a>
  */
-public class Main extends JApplet {
+public class Main extends JApplet implements ActionListener{
   private static final long serialVersionUID = 1L;
-
+    JButton jOpenButton = new JButton();
+    JButton jNewButton = new JButton();
+    JTextPane editorPane=new JTextPane();
+    JFileChooser fc;
   // This is the way to build the applet
   public void init() {
   // Panes
@@ -34,22 +47,34 @@ public class Main extends JApplet {
     JPanel panetop=new JPanel();
     panetop.setLayout(new BorderLayout());
     JToolBar tools=new JToolBar();
-    JTextPane editorPane=new JTextPane();
+    
   // Panes positions
     pane.add(panetop,BorderLayout.NORTH);
     panetop.add(tools,BorderLayout.WEST);
     pane.add(editorPane,BorderLayout.CENTER);
   // Butons
-    JButton jNewButton = null;
-    jNewButton = new JButton();
+    fc = new JFileChooser();
     jNewButton.setIcon(Utils.getIcon("org/javascool/doc-files/icones16/New_16x16.png"));
-    JButton jStopButton = null;
-    jStopButton = new JButton();
-    jStopButton.setIcon(Utils.getIcon("org/javascool/doc-files/icones16/Stop_16x16.png"));
     tools.add(jNewButton);
+    jNewButton.addActionListener(this);
+    jOpenButton.setIcon(Utils.getIcon("org/javascool/doc-files/icones16/Open_16x16.png"));
+    jOpenButton.addActionListener(this);
+    tools.add(jOpenButton);
+    JButton jStopButton = new JButton();
+    jStopButton.setIcon(Utils.getIcon("org/javascool/doc-files/icones16/Stop_16x16.png"));
     tools.add(jStopButton);
   // Set Visible
     setContentPane(pane);
+  }
+  
+  public void actionPerformed(ActionEvent e){
+    if(e.getSource()==jOpenButton){
+    fc.showOpenDialog(Main.this);
+    File file = fc.getSelectedFile();
+    editorPane.setText("Vous avez selectionnez : "+file.getName()+"\nQui est dans : "+file.getPath());
+    }
+    else if(e.getSource()==jNewButton){System.out.println("Hi ! V4 is comming :-)");}
+    else{System.out.println("ERROR");}
   }
 
   /** Used to run a javasccol v3 as a standalone program. 
