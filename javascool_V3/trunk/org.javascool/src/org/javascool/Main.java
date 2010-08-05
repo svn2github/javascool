eaa494db24f99d547647689bbfd59972d54de637
philien <philien@79b98d2d-744f-0410-929f-8ac7b80d50f7>
2010-08-05 06:15:26 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/javascool@534 79b98d2d-744f-0410-929f-8ac7b80d50f7

--- javascool_V3/trunk/org.javascool/src/org/javascool/Main.java
+++ javascool_V3/trunk/org.javascool/src/org/javascool/Main.java
@@ -9,8 +9,13 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JToolBar;
+import javax.swing.JTextPane;
+import javax.swing.JTabbedPane;
 import javax.swing.ImageIcon;
+import javax.swing.JComponent;
+import javax.swing.JSplitPane;
 
+import java.lang.String;
 import java.awt.BorderLayout;
 
 /** This is the javascool v3 interface starter.
@@ -24,24 +29,31 @@ public class Main extends JApplet {
   // This is the way to build the applet
   public void init() {
   // Butons declarations
-    JButton jDemoButton = null;
-    jDemoButton = new JButton();
-    jDemoButton.setIcon(Utils.getIcon("org/javascool/doc-files/Play_32x32.png"));
+    JButton jNewButton = null;
+    jNewButton = new JButton();
+    jNewButton.setIcon(Utils.getIcon("org/javascool/doc-files/New_32x32.png"));
     JButton jStopButton = null;
     jStopButton = new JButton();
     jStopButton.setIcon(Utils.getIcon("org/javascool/doc-files/Stop_32x32.png"));
+    JTextPane editorPane = new JTextPane();
   // Creation of Panel
     JPanel pane = new JPanel();
+    JPanel pane2 = new JPanel();
   // Setting of layouts
     pane.setLayout(new BorderLayout());
   // Creation of toolbar
     JToolBar bar = new JToolBar();
+    JPanel panetop = new JPanel();
+    JSplitPane split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,pane2,editorPane);
   // Adding of all to the toolbar
-    pane.add(bar, BorderLayout.NORTH);
-    bar.add(jDemoButton, BorderLayout.EAST);
+    pane.add(split);
+    panetop.add(jNewButton);
     bar.add(jStopButton);
+    pane.add(bar,BorderLayout.NORTH);
+    pane.add(panetop, BorderLayout.APPLET);
   // Show Panel
     setContentPane(pane);
+
   }
 
   /** Used to run a javasccol v3 as a standalone program. 
