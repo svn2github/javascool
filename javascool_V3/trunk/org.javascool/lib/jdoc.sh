3ffc4818ac4ae4a8935db63afe32edcf968d9449
vthierry <vthierry@79b98d2d-744f-0410-929f-8ac7b80d50f7>
2010-08-19 09:40:09 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/javascool@719 79b98d2d-744f-0410-929f-8ac7b80d50f7

--- javascool_V3/trunk/org.javascool/lib/jdoc.sh
+++ javascool_V3/trunk/org.javascool/lib/jdoc.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
 # Usage of this script
-if [ -z "$1" -o "$1" = "-h" -o "$1" = "--help" ] 
+if [ "$1" = "-h" -o "$1" = "--help" ] 
 then cat <<EOD
-Usage: $0 
+Usage: $0 [JDOCLOCATION]
   JDoc generator
 EOD
 exit 0
 fi
 
-# Defines the jdoc target directory
-jdoc=/tmp/javascool/jdoc
-#jdoc=/usr/local/javascool/jdoc
-echo "                    JDoc Generator                      "
+# Defines the jdoc target directory (writable by all tu update the APIJVS)
+if [ -z "$1" ] ; then jdoc=/tmp/javascool/jdoc ; else jdoc=$1 ; fi
+mkdir -p $jdoc/api $jdoc/APIJVS
+chmod a+w $jdoc/APIJVS
+
+echo "             JDoc Generator and Viewer                     "
 echo "  Welcome to the generator of the bigest Java Doc"
 echo ""
 echo " --------------------------------------------------------"
@@ -24,10 +26,9 @@ echo " Distributed on GNU GPL V3                      "
 echo " For more information : http://javascool.gforge.inria.fr"
 echo " ________________________________________________________"
 echo " "
+echo "> jdoc is in $jdoc"
 echo "> Evaluate and build dependances ..."
-mkdir -p $jdoc/api $jdoc/APIJVS
-chmod a+w $jdoc/APIJVS # pour pouvoir le faire en tant que user mmm si root
-make -f - $jdoc/api/doc.tar.gz $jdoc/base.tar.gz <<EOF
+make -s -f - $jdoc/api/doc.tar.gz $jdoc/base.tar.gz <<EOF
 
 $jdoc/api/doc.tar.gz:
 	@echo "You need to build the Java Api Doc :  (This can take some time)"
@@ -48,17 +49,8 @@ EOF
 echo "Good, dependences are ok and we now build the Javascool API Private Doc"
 echo "> Generating the JavaScool API Private Doc ..."
 if echo $0 | grep '^/' ; then jroot="$0" ; else jroot="`pwd`/$0" ; fi ; jroot=`echo $jroot | sed 's/\/lib\/jdoc.sh$//' | sed 's/\/lib\/\.\/jdoc.sh$//'`
-make -f - $jdoc/APIJVS <<EOF
-SRC = $(shell find $jroot/src -name '*.java')
-
-$jdoc/APIJVS : \$(SRC)
-	@echo "> Coping doc-file to the JavaScool API Doc ..."
-	@$(MAKE) -C $jroot api
-	@cp -r www/api/* \$@
-	@ echo "> Generating the JavaScool API Private Doc ..."
-	@javadoc -quiet -private -nohelp -nodeprecated -notree -noindex -author -charset "UTF-8" -use -linkoffline ../api/ $jdoc/api/ -windowtitle "Java's Cool v3" -d $jdoc/APIJVS \$(SRC)
-EOF
-echo "Fine, JDoc is generated. Its size is `du -s -B 1048576 $jdoc | awk '{print $$1}'`Mbytes"
+make -s -C $jroot JDOC=$jdoc api
+echo "Fine, JDoc is generated. Its size is `du -s -B 1048576 $jdoc | awk '{print $1}'`Mbytes"
 
 echo "Let us open JDoc, open in your browser: Read The Fine Manuel (RTFM)"
 firefox $jdoc/index.html 
