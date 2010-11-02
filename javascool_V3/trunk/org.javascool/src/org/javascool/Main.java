fb5d28b387061223c612166dd0156c2810ce3c1d
philien <philien@79b98d2d-744f-0410-929f-8ac7b80d50f7>
2010-11-02 12:23:03 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/javascool@929 79b98d2d-744f-0410-929f-8ac7b80d50f7

--- javascool_V3/trunk/org.javascool/src/org/javascool/Main.java
+++ javascool_V3/trunk/org.javascool/src/org/javascool/Main.java
@@ -84,9 +84,11 @@ public class Main extends JApplet {
   private JToolBar toolBar = new JToolBar(title, JToolBar.HORIZONTAL);
   private JTabbedPane westPane = new JTabbedPane(), eastPane = new JTabbedPane();
   private JComboBox activityList = new JComboBox();
+  private JsFileChooser fileChooser;
   /** Builds the GUI. */
   private void initGUI() {
     JPanel toppane = new JPanel();
+    fileChooser = new JsFileChooser();
     toppane.setLayout(new BorderLayout());
     toolBar.setBorderPainted(false);
     JPanel toppaneWest = new JPanel();
@@ -295,99 +297,6 @@ public class Main extends JApplet {
   private boolean firstActivity = true;
 
   // [2] File Open/Save management and Help
-  
-  private class JsFileChooser extends JFileChooser {
-    private static final long serialVersionUID = 1L;
-    // Initializes the file chooser
-    {
-      // Use the user.dir to start the interaction
-      setCurrentDirectory(new File(System.getProperty("user.dir")));
-      // Defines a Jvs/Pml extension filter
-      setFileFilter(new FileFilter() {
-	  public String getDescription() { 
-	    return extension == null ? "Tous les Fichiers" : "Fichiers " + extension.substring(1).toUpperCase();
-	  }
-	  public boolean accept(File file) {
-	    return file.isDirectory() ? true : extension == null ? true : file.getName().endsWith(extension);
-	  }
-	});
-    }
-    private String extension = null;
-    /** Gets the last selected file. */ 
-    public String getFile() { return file; } private String file;
-    /** Resets the selected file. */
-    public void resetFile() { file = null; }
-    /** Manages an open dialog action. 
-     * @param editor The editor where to load the file.
-     * @param extension The required file extension, if any (else null).
-     */
-    public void doOpenAs(Editor editor, String extension) {
-      if (this.extension != extension) setSelectedFile(null);
-      this.extension = extension;
-      setDialogTitle("Ouvrir un programme");
-      setDialogType(JFileChooser.OPEN_DIALOG);
-      setApproveButtonText("Ouvrir");
-      if (showOpenDialog(Main.this) == 0)
-	doOpen(editor, getSelectedFile().getPath());
-    }
-    /** Manages an open action (no dialog). */
-    public void doOpen(Editor editor, String file) {
-      setSelectedFile(new File(file));
-      String text = ""; try { this.file = file; text = Utils.loadString(file); } catch(Exception e) { }
-      editor.setText(text);
-    }
-    /** Manages a save dialog action. 
-     * @param editor The editor from where the file is saved.
-     * @param extension The required file extension, if any (else null).
-     * @return True if the dialog is validated, else false.
-     */
-    public boolean doSaveAs(Editor editor, String extension) {
-      if (this.extension != extension) setSelectedFile(null);
-      this.extension = extension;
-      if (title == null) title = "Enregister un programme";
-      setDialogTitle(title);
-      title = null;
-      setDialogType(JFileChooser.SAVE_DIALOG);
-      setApproveButtonText("Enregister");
-      if (showSaveDialog(Main.this) == 0) {
-	file = getSelectedFile().getPath();
-	doSave(editor, extension);
-	return true;
-      } else 
-	return false;
-    }
-    /** Manages a save action (no dialog). 
-     * @param editor The editor from where the file is saved.
-     * @param extension The required file extension, if any (else null).
-     */
-    public void doSave(Editor editor, String extension) {
-      if (file == null) {
-	doSaveAs(editor, extension);
-      } else { 
-	doSave(editor, editor.getText(), extension);
-      }
-    }
-    /** Manages a save action (no dialog). 
-     * @param editor The editor where the text comes from.
-     * @param text The text to save.
-     * @param extension The required file extension, if any (else null).
-     */
-    public void doSave(Editor editor, String text, String extension) {
-      // Normalizes the file name and extension
-      file = toJavaName(file, extension); setSelectedFile(new File(file));
-      // Cleans spurious chars
-      text = text.replaceAll("[\u00a0]", "");
-      // Adds a new line if not yet done
-      text = "\n"+text.trim();
-      // Reload in editor the clean text
-      if (editor != null) editor.setText(text);
-      Utils.saveString(file, text);
-    }
-    /** Sets the next save dialog title. 
-     * @param title Optional title for a specific dialog
-     */
-    public void setSaveDialogTitle(String title) { this.title = title; } private String title = null;
-  }
   // Corrects the file name if not standard and cancel the extension
   private static String toJavaName(String file, String extension) {
     File f = new File(file);
@@ -404,7 +313,7 @@ public class Main extends JApplet {
     return parent + File.separator + main + extension;
   }
 
-  private JsFileChooser fileChooser = new JsFileChooser();
+  
   private Runnable fileOpen = new Runnable() { public void run() {
     if (activity == null) return;
     switch(!activity.getEditor().isModified() ? 1 : new JOptionPane().
