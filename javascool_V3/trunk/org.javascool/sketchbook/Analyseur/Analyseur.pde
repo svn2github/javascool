0639ca98d35d9e7566a1ce82db150418aff333e7
cpicard <cpicard@79b98d2d-744f-0410-929f-8ac7b80d50f7>
2010-11-26 13:32:45 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/javascool@1051 79b98d2d-744f-0410-929f-8ac7b80d50f7

--- javascool_V3/trunk/org.javascool/sketchbook/Analyseur/Analyseur.pde
+++ javascool_V3/trunk/org.javascool/sketchbook/Analyseur/Analyseur.pde
@@ -36,11 +36,6 @@ void setup()
   in = minim.getLineIn(Minim.STEREO, 512);
 
   nb = 128;
-  
-  fft = new FFT(in.bufferSize(), in.sampleRate());
-  // use 'nb' averages (between 64 and 1024).
-  // the maximum number of averages we could ask for is half the spectrum size. 
-  fft.linAverages(nb);
 
   rectMode(CORNERS);
 }
@@ -48,26 +43,16 @@ void setup()
 void draw()
 {
   fill(255);
-  if(path != null) {
-    fft = new FFT(jingle.bufferSize(), jingle.sampleRate());
-    fs = jingle.sampleRate();
-    fft.linAverages(nb);
-    fft.forward(jingle.mix);
-  } 
-  else {
-    fft.forward(in.mix);
-  }
-  int w = int(fft.specSize()/nb);
+  
+  update();
+  
+  printInfo();
+ 
+  
+}
 
-  for(int i = 0; i < fft.avgSize(); i++)
-  {
-    // draw a rectangle for each average, multiply the value by 5 so we can see it better
-    rect(i*w, height, i*w + w, height - fft.getAvg(i)*5);
-  }
-  
-  
-  
-  // Info
+void printInfo() {
+
   if(info) {
     fill(0);
     rect(0, 100, width/2+200, -10);
@@ -75,9 +60,9 @@ void draw()
     textAlign(LEFT);
     fill(255, 150, 0);
     text(" - COMMANDES - \n " + 
-      ".  L / l : chargement d'un fichier son:  accès dans une base de données \n " +
+      ".  L / l : chargement d'un fichier son (Nina Hagen, Born in Xixas) \n " +
       ".  + / - : raffine/élargit les bandes de fréquences pour la FFT (entre 64 et 256) \n " +
-      ".  S / s : stop/redémarre la lecture \n " +
+      ".  S / s : stop la lecture, analyse de l'entrée micro \n " +
       ".  I / i : afficher/cacher les instructions \n " +
       ".  ESC: Fermer l'application ", 10, 15);
 
@@ -88,24 +73,49 @@ void draw()
       textAlign(LEFT);
       fill(255, 150, 0);
       text("'i' pour info ", 10, 15);
+  } 
+}
+
+void update() {
+    int coeff;
+    if(path != null) {
+    fft = new FFT(jingle.bufferSize(), jingle.sampleRate());
+    fs = jingle.sampleRate();
+    fft.linAverages(nb);
+    fft.forward(jingle.mix);
+    coeff = 1;
+  } 
+  else {
+    fft = new FFT(in.bufferSize(), in.sampleRate());
+    fft.linAverages(nb);
+    fft.forward(in.mix);
+    coeff = 5;
   }
-  
-  
+  int w = int(fft.specSize()/nb);
+
+  for(int i = 0; i < fft.avgSize(); i++)
+  {
+    // draw a rectangle for each average, multiply the value by 5 so we can see it better
+    rect(i*w, height, i*w + w, height - coeff*fft.getAvg(i));
+  }
+    
 }
 
 void mouseMoved() {
   int w = int((fs+nb)/(nb));
-  println("/ " + nb+ "/ " +fft.avgSize() + "/ " +fft.specSize());
+  //println("/ " + nb+ "/ " +fft.avgSize() + "/ " +fft.specSize());
   textFont(f,12);
   fill(0);
   rect(width, 150, width/2+80, height/3);
   fill(255);
   text("Fréquence pointée = " + w*(fft.avgSize()*mouseX)/width, width-170, int(height/2));
 }
+
 void keyPressed()                                                  
 {
   if (key == '+') {
-    if(nb<256) {
+    // use 'nb' averages (between 32 and 256).
+    if(nb<128) {
       background(0);
       nb*=2;
     } else {
@@ -121,8 +131,9 @@ void keyPressed()
     background(0);
     String pathN;
 
-    pathN = selectInput();
-    if(pathN != null) {
+    //pathN = selectInput();
+    pathN = "NinaHagen_BornInXixas_ext.mp3";
+    if(pathN != null && !(sounding)) {
       path = pathN;
       jingle = minim.loadFile(path);
       sounding = true;
@@ -141,13 +152,11 @@ void keyPressed()
     if(sounding) {
       jingle.pause();
       sounding = false;
-    } else {
-      if(path != null) {
-        jingle = minim.loadFile(path);
-        sounding = true;
-        jingle.loop();
-      }
-    }
+      path = null;
+      fill(0);
+      rect(0, height, width/2+200, -10);
+      update();
+    } 
   }
 }
 
--- javascool_V3/trunk/org.javascool/www/sketchbook/Analyseur/Analyseur.java
+++ javascool_V3/trunk/org.javascool/www/sketchbook/Analyseur/Analyseur.java
@@ -59,11 +59,6 @@ public void setup()
   in = minim.getLineIn(Minim.STEREO, 512);
 
   nb = 128;
-  
-  fft = new FFT(in.bufferSize(), in.sampleRate());
-  // use 'nb' averages (between 64 and 1024).
-  // the maximum number of averages we could ask for is half the spectrum size. 
-  fft.linAverages(nb);
 
   rectMode(CORNERS);
 }
@@ -71,26 +66,16 @@ public void setup()
 public void draw()
 {
   fill(255);
-  if(path != null) {
-    fft = new FFT(jingle.bufferSize(), jingle.sampleRate());
-    fs = jingle.sampleRate();
-    fft.linAverages(nb);
-    fft.forward(jingle.mix);
-  } 
-  else {
-    fft.forward(in.mix);
-  }
-  int w = PApplet.parseInt(fft.specSize()/nb);
+  
+  update();
+  
+  printInfo();
+ 
+  
+}
 
-  for(int i = 0; i < fft.avgSize(); i++)
-  {
-    // draw a rectangle for each average, multiply the value by 5 so we can see it better
-    rect(i*w, height, i*w + w, height - fft.getAvg(i)*5);
-  }
-  
-  
-  
-  // Info
+public void printInfo() {
+
   if(info) {
     fill(0);
     rect(0, 100, width/2+200, -10);
@@ -98,9 +83,9 @@ public void draw()
     textAlign(LEFT);
     fill(255, 150, 0);
     text(" - COMMANDES - \n " + 
-      ".  L / l : chargement d'un fichier son:  acc\u00e8s dans une base de donn\u00e9es \n " +
+      ".  L / l : chargement d'un fichier son (Nina Hagen, Born in Xixas) \n " +
       ".  + / - : raffine/\u00e9largit les bandes de fr\u00e9quences pour la FFT (entre 64 et 256) \n " +
-      ".  S / s : stop/red\u00e9marre la lecture \n " +
+      ".  S / s : stop la lecture, analyse de l'entr\u00e9e micro \n " +
       ".  I / i : afficher/cacher les instructions \n " +
       ".  ESC: Fermer l'application ", 10, 15);
 
@@ -111,24 +96,49 @@ public void draw()
       textAlign(LEFT);
       fill(255, 150, 0);
       text("'i' pour info ", 10, 15);
+  } 
+}
+
+public void update() {
+    int coeff;
+    if(path != null) {
+    fft = new FFT(jingle.bufferSize(), jingle.sampleRate());
+    fs = jingle.sampleRate();
+    fft.linAverages(nb);
+    fft.forward(jingle.mix);
+    coeff = 1;
+  } 
+  else {
+    fft = new FFT(in.bufferSize(), in.sampleRate());
+    fft.linAverages(nb);
+    fft.forward(in.mix);
+    coeff = 5;
   }
-  
-  
+  int w = PApplet.parseInt(fft.specSize()/nb);
+
+  for(int i = 0; i < fft.avgSize(); i++)
+  {
+    // draw a rectangle for each average, multiply the value by 5 so we can see it better
+    rect(i*w, height, i*w + w, height - coeff*fft.getAvg(i));
+  }
+    
 }
 
 public void mouseMoved() {
   int w = PApplet.parseInt((fs+nb)/(nb));
-  println("/ " + nb+ "/ " +fft.avgSize() + "/ " +fft.specSize());
+  //println("/ " + nb+ "/ " +fft.avgSize() + "/ " +fft.specSize());
   textFont(f,12);
   fill(0);
   rect(width, 150, width/2+80, height/3);
   fill(255);
   text("Fr\u00e9quence point\u00e9e = " + w*(fft.avgSize()*mouseX)/width, width-170, PApplet.parseInt(height/2));
 }
+
 public void keyPressed()                                                  
 {
   if (key == '+') {
-    if(nb<256) {
+    // use 'nb' averages (between 32 and 256).
+    if(nb<128) {
       background(0);
       nb*=2;
     } else {
@@ -144,8 +154,9 @@ public void keyPressed()
     background(0);
     String pathN;
 
-    pathN = selectInput();
-    if(pathN != null) {
+    //pathN = selectInput();
+    pathN = "NinaHagen_BornInXixas_ext.mp3";
+    if(pathN != null && !(sounding)) {
       path = pathN;
       jingle = minim.loadFile(path);
       sounding = true;
@@ -164,13 +175,11 @@ public void keyPressed()
     if(sounding) {
       jingle.pause();
       sounding = false;
-    } else {
-      if(path != null) {
-        jingle = minim.loadFile(path);
-        sounding = true;
-        jingle.loop();
-      }
-    }
+      path = null;
+      fill(0);
+      rect(0, height, width/2+200, -10);
+      update();
+    } 
   }
 }
 
