bbbf2fdbda06892ae8c90c6a9dc0ca98b0735437
vthierry <vthierry@79b98d2d-744f-0410-929f-8ac7b80d50f7>
2011-03-03 13:20:22 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/javascool@1165 79b98d2d-744f-0410-929f-8ac7b80d50f7

--- javascool_V3/trunk/org.javascool/lib/javascool.bat
+++ javascool_V3/trunk/org.javascool/lib/javascool.bat
@@ -1,3 +1,4 @@
 rem Fichier de lancement de javascool sous windows pour rendre visible les erreurs d execution
-"C:\Program Files\Java\jre6\bin\java" -jar javascool.jar
+java -jar javascool-3-2.jar
+rem "C:\Program Files\Java\jre6\bin\java" -jar javascool.jar
 pause
