f264e170fba6424fe419badd0608fe68cc911571
vthierry <vthierry@79b98d2d-744f-0410-929f-8ac7b80d50f7>
2011-04-27 18:35:25 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/javascool@1254 79b98d2d-744f-0410-929f-8ac7b80d50f7

--- javascool_V3/trunk/org.javascool/src/org/javascool/JsProglet.java
+++ javascool_V3/trunk/org.javascool/src/org/javascool/JsProglet.java
@@ -6,78 +6,82 @@ package org.javascool;
 
 import javax.swing.JPanel;
 
-/** Define a «proglet», i.e. a piece of code that can be integrated in JavaScool. 
- * - This specification is a draft for Javascool3.3 : DO NOT USE yet !!!
+/** Définit une «proglet», c'est à dire un objet numérique programmaable qui, ici, s'intègre dans JavaScool.
+ * <pre>- This specification is a draft for Javascool3.3 : DO NOT USE YET !!!</pre>
+ * <ul>
+ * <li><b>Spécification d'une «proglet».</b><ul>
+ *   <li>Chaque «proglet» doit avoir un <i>nom</i> différent: <ul>
+ *      <li>c'est celui de la classe Java qui définit la «proglet».</li>
+ *      <li>c'est aussi celui du répertoire qui contient les éléments qui constituent la «proglet».</li>
+ * </ul></li>
+ *  <li>La classe Java qui définit la «proglet» doit: <ol>
+ *    <li>Définir la variable statique <a href="#progletProperties"><tt>public static String progletProperties;</tt></a> qui contient les méta-données permettant d'intégrer la  «proglet». <ul>
+ *       <li>C'est ce qui indique que cette classe définit une  «proglet».</li>
+ *    </ul></li>
+ *    <li>Définir la variable statique <tt>public static javax.swing.JPanel panel</tt> si l'objet numérique s'incarne dans un objet graohique.</li>
+ *    <li>Définir la méthode statique <tt>public static void test();</tt> si l'objet numérique a un petit code de démo de son fonctionnement.</li>
+ * </ol></li>
+ * <li>Les éléments qui définissent une «proglet»: <ul>
+ *   <li> Sont les fichiers de code, documentation, multimédia (image, son, etc..), regroupés dans un répertoire qui porte le nom de la «proglet».</li>
+ *   <li> Les fichiers de documents XML utilisent les constructions de <a href="doc-files/about-hdoc.htm">Hdoc</a> et <a href="doc-files/about-hml.htm">Hml</a>.</li>
+ *   <li> Les très gros fichiers sont à poser sur le Web sans imposer de les charger comme ressources de la «proglet».</li>
+ *   <li> Le répertoire peut contenu d'autres fichiers JAR si des librairies externes sont utilisées.</li>
+ *  </ul></li>
+ * </ul></li>
+ * <li><b>Manipulation de l'objet numérique programmable.</b><ul>
+ *  <li>Toutes les méthodes accessibles à l'utilisateur pour manipuler l'objet numérique sont définies par des méthodes <tt>public static</tt>,
+ *    qui seront appellés en les préfixant  du nom de la «proglet» (par exemple <tt>Scope.clear();</tt> pour effacer l'oscilloscope.</li>
+ *  <li>Il ne faut jamais [re]définir de méthodes accessibles à l'utilisateur de nom: 
+ *     <tt>clone</tt>, <tt>equals</tt>, <tt>finalize</tt>, <tt>getClass</tt>, <tt>hashCode</tt>, <tt>notify</tt>, <tt>run</tt>, <tt>toString</tt>, ou <tt>wait</tt>.</li>
+ *  <li>Une bonne méthode pour écrire une proglet est de partir d'un exemple et de l'adapter aux nouvelles fonctionnalités souhaitées.</li>
+ * </ul></li>
+ * <li><b>Intégration automatique d'une proglet dans JavaScool.</b> Lors de son lancement, JavaScool: <ul>
+ *   <li>regarde dans les sous-répertoires de son répertoire de lancement les fichiers Java qui contiennent des classes avec <tt>public static String progletProperties;</tt>,
+ *      donc des  «proglet».</li>
+ *   <li>compile les fichiers Java et XML de ce répertoire et: <ol>
+ *     <li>soit rapporte les erreurs de compilation ou d'intégration si il y en a</li>
+ *     <li>soit génère un fichier JAR avec tous les éléments de la  «proglet».</li>
+ *   </ol></li>
+ *   <li>charge ensuite toutes les  «proglet» visibles dans son environnement.</li>
+ * </ul>
+ *
+ * <pre>Notes critiques de vthierry:
+ * - c'est lourd de définir progletProperties; comme une string multi-ligne en java (faut mettre plein de ""+\n""+ . . ) on pourrait mettre la string dans la doc mais c'est intrduire une bricole/verrue
+ * - En JsProglet sert a rien puisque c'est juste un "exemple vide" 
+ * - La mecanique de chargement dynaamique de proglet est pas gagnee . . a revoir 
+ * </pre>
  */
 public class JsProglet {
-  /** Returns the «proglet» meta-data.
-   * @return A String of the form :<tt>"{<br>
-   *  title = '<i>the «proglet» title</i></i>'<br>
-   *  description = '<i>a one/two line(s) description of what the «proglet» proposes</i>'<br>
-   *  category = '<i>the «proglet» category (see below)</i>'<br>
-   *  vendor = '<i>the contact email address or home page url</i>'<br>
-   *  type = '<i>the «proglet» type</i>'<br>
-   *  width = '<i>the «proglet» width, if constrainted</i>'<br>
-   *  height = '<i>the «proglet» height, if constrainted</i>'<br>
-   *  pages = {<br>
-   *   { name = 'usage' file = 'proglet/<i>the-proglet-name</i>/doc-files/about-proglet.htm' } // Description of the « proglet » routines »<br>
-   *   { name = 'activité' file = 'proglet/<i>the-proglet-name</i>/doc-files/<i>file-name</i>.htm' } // Description of some activity with the proglet<br>
-   *   { name = '<i>the document page title</i>' file = '<i>the document page file relative location of the form: </i>proglet/<i>the-proglet-name</i>/doc-files/<i>the-file-name</i>.htm' }<br>
-   *   // etc..<br>
-   *  }<br>
-   *  video = '<i>an URL where a video demo is available</i>'<br>
-   *  applet = '<i>an URL where the «proglet» is available as a web applet</i>' // If not a standard page is generated ob the web site.<br>
-   * }"</tt><br>
-   * Remarks: <ul>
-   * <li>The «proglet» name is the Java class name.</li>
-   * <li>Standard «proglet» categories are:
+  private JsProglet() {}
+  /** Définit les méta-donnęes de la «proglet».
+   * C'est une String de syntaxe :<tt><b>"{<br>
+   *  &nbsp;&nbsp; title = '<i>le titre de la «proglet»</i></i>'<br>
+   *  &nbsp;&nbsp; description = '<i>une à deux lignes de description de ce que la «proglet» propose</i>'<br>
+   *  &nbsp;&nbsp; category = '<i>la catégorie pédagogique de la «proglet» (voir ci-dessous)</i>'<br>
+   *  &nbsp;&nbsp; vendor = '<i>le contact email ou la page web pour contacter l'auteur de la proglet</i>'<br>
+   *  &nbsp;&nbsp; type = '<i>le type de «proglet»  (voir ci-dessous)</i>'<br>
+   *  &nbsp;&nbsp; width = '<i>la largeur en pixel de la «proglet», si la valeur est contrainte</i>'<br>
+   *  &nbsp;&nbsp; height = '<i>la hauteur en pixel de la «proglet», si la valeur est contrainte</i>'<br>
+   *  &nbsp;&nbsp; pages = { // Documentation de la «proglet»<<br>
+   *  &nbsp;&nbsp;&nbsp;&nbsp; { name = 'usage' file = '<i>the-proglet-name</i>/doc-files/about-proglet.htm' } // Description des routines de la «proglet»<br>
+   *  &nbsp;&nbsp;&nbsp;&nbsp; { name = 'activité' file = '<i>the-proglet-name</i>/doc-files/<i>file-name</i>.htm' } // Description de l'activité réalisée avec la «proglet»<br>
+   *  &nbsp;&nbsp;&nbsp;&nbsp; { name = '<i>the document page title</i>' file = '<i>the-proglet-name</i>/doc-files/<i>the-file-name</i>.htm' } // Autres documents relatifs à la «proglet»<br>
+   *  &nbsp;&nbsp; &nbsp;&nbsp;// etc..<br>
+   *  &nbsp;&nbsp; }<br>
+   *  &nbsp;&nbsp; video = '<i>une URL où une vidéo de démonstration est disponible</i>'<br>
+   *  &nbsp;&nbsp; applet = '<i>une URL où la «proglet» focntionne en "educlet" sur le Web</i>'<br>
+   * }"</b></tt><br>
+   * Remarques: <ul>
+   * <li>Le nom de la  «proglet» c'est pas redonnée ici car c'est le nom de la classe Java et du répertoire des ressources.</li>
+   * <li>Les catégories pédagogiques standards sont:
    * <br> "Apprendre à programmer", "Algorithmes dichotomiques", "Aller plus loin en programmation", "Objet numérique: le son", "Objet numérique: l'image", "Objet numérique: le graphe".</li>
-   * <li>The «proglet» type is either "processing" for «proglets»  built from the processing platform, "algobox" for proglets using the algo-editor, or "java" (default type).</li>
-   * <li>Width and height must be specified for "processing" type «proglet».</li>
+   * <li>Le type d'une «proglet» est soit "processing" pour des «proglets» construites à partir de processing, "algobox" pour des «proglets» utilisant l'"algo-editor", ou "java" (type par défaut).</li>
+   * <li>Les width et height sont à spécifier pour des «proglet» de type "processing".</li>
    * </ul>
    */
-  static String progletProperties = "{}";
-  /** References the panel corresponding to the «proglet» graphical interface if any (if only using the console, return null or undefined). */
-  static final JPanel panel = null;
-  /** Runs a test/demo of the «proglet» graphical interface if any (no demo if undefined). */
-  static final void test() { }
+  public static String progletProperties = "{}";
+  /** Référence le panel correspondant à l'interface graphique de la «proglet», si il existe (si seule la console est utilisée, le champ peut rester indéfini). */
+  public static final JPanel panel = null;
+  /** Lance un test/demo sur l'interface graphique de la «proglet», si définit (si il n'y a pas de démo, la méthode peut rester indéfinie). */
+  public static final void test() { }
 }
-
-/** Indique que la classe est une proglet.
- *  <p>Par contrat, une <a href="doc-files/about-proglets.htm">«proglet»</a> doit: <ul>
- *    <li>Implementer l'interface <tt>org.javascool.Proglet</tt>.</li>
- *    <li>Définir un <tt>public static final JPanel panel;</tt> qui est l'instanciation graphique de la proglet.</li>
- *    <li>Définir une méthode <tt>static void test()</tt> pour fournir un test/démonstration de la proglet.</li>
- *    <li>Définir toutes les méthodes accessibles à l'utilisateur sous la forme de méthode <tt>public static</tt> préfixées par le nom de la proglet.</li>
- *    <li>Ne jamais [re]définir de méthodes accessibles à l'utilisateur de nom:
- *       <tt>clone</tt>, <tt>equals</tt>, <tt>finalize</tt>, <tt>getClass</tt>, <tt>hashCode</tt>, <tt>notify</tt>, <tt>run</tt>, <tt>toString</tt>, ou <tt>wait</tt>,
- *       réservé pour les objets <tt>Runnable</tt> de <tt>Java</tt> utilisés ici pour l'encapsulation.</li>
- *    <li>Avoir un constructeur <tt>private</tt> puisque c'est une factory avec uniquement des méthodes statiques qui ne peut être construit.</li>
- *  </ul></p>
- *  <p>Une bonne méthode pour écrire une proglet est de partir d'un exemple et de l'adapter aux nouvelles fonctionnalités souhaitées.</p>
- *  <p>Ces <a href="doc-files/about-proglets.htm">«proglets»</a> sont écrites pour permettre facilement à un enseignant ou un partenaire,
- *    qui programme en Java au niveau élémentaire d'adapter, enrichir ces éléments.</p>
- *  <p>Non sans les <a href="http://javascool.gforge.inria.fr/?page=contact">partager</a> avec nous tout(te)s en nous contactant.</p>
- *
- * <hr/>Intégration de la proglet dans javascool:<ol>
- *  <li>Les fichiers sources sont dans un répertoire: <tt>src/proglet/<i>nom-de-la-proglet</i>/*.java</tt></li>
- *  <li>Les fichiers de documentation et de description des activités sont dans un répertoire: <tt>src/proglet/<i>nom-de-la-proglet</i>/doc-files/*.(xml|png|..)</tt>:<ul>
- *    <li>Les fichiers de documentation sont en XML avec les constructions de <a href="doc-files/about-hdoc.htm">Hdoc</a> et <a href="doc-files/about-hml.htm">Hml</a>.</li>
- *    <li>Un fichier de documentation de la proglet est créé en <tt>src/proglet/<i>nom-de-la-proglet</i>/doc-files/about-proglet.xml</tt>.</li>
- *    <li>Un fichier de lancement de la proglet est créé en <tt>src/proglet/<i>nom-de-la-proglet</i>/doc-files/the-proglet.xml</tt>:
- *      <li>Il contient la directive <tt>&lt;p>&lt;l class="proglet" link="<i>nom-de-la-proglet</i>"/>&lt;/p></tt>.</li>
- *   </ul></li>
- *  </ul></li>
- *  <li>Le fichier <tt>src/org/javascool/doc-files/about-proglets.xml</tt> contient la <a href="doc-files/about-proglets.htm">table de toutes les proglets</a>.</li>
- * </ol>
- */
-/*
-
- - Ajouter un integrateur de proglet: compile les xml en htm, hurle si fichier kk, genere un jar signé.
- - Reconnaitre les proglets : par declaration, par lecture des fichiers du path ? de javascool.jar ?
- - Reunit les proglets/processing proglets en faisant un lien de sketchbook dans proglet
- - Ah que les prglet pas dans progtlet pour defalut et puis voila ?
- - Ah que Proglet si et seulement si a les fields progletProperties etc . . ?
- - Creer un org.javascool.core ? org.javascool.widget ?
-
-*/
