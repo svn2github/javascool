19cbebfa3c8edce9dc6e3a024b6d3645273c5144
vthierry <vthierry@79b98d2d-744f-0410-929f-8ac7b80d50f7>
2015-11-09 09:16:26 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/javascool@1293 79b98d2d-744f-0410-929f-8ac7b80d50f7

--- javascool_V4/web/pages/run/display.php
+++ javascool_V4/web/pages/run/display.php
@@ -19,6 +19,8 @@ Il suffit de le télécharger <b><a href="javascool-proglets.jar">ICI</a></b> ou
 
     <p>Pour une version antérieure java 1.6 essayer la version <tt>javascool-proglets-6.jar</tt> disponible en <b><a href="http://javascool.gforge.inria.fr/v4/javascool-proglets-6.jar">cliquant ici</a></b></p>
 
+    <p>ATTENTION: Javascool ne marche pas [encore] sous Windows10, désolé, nous sommes sur le problème.</p>
+
 <br/>
 
     <p>- Si le lancement échoue, vous devrez probablement <a href="http://www.java.com/fr/download">installer Java<a/>.</p>
